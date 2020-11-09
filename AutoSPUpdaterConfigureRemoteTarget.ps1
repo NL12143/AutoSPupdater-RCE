62330959807b7fc1e4852b865362b397e34afee3
Roeland Cerfonteijn <NL12143@hotmail.com>
2020-11-09 21:51:42 +0100

    Update AutoSPUpdaterConfigureRemoteTarget.ps1

--- AutoSPUpdaterConfigureRemoteTarget.ps1
+++ AutoSPUpdaterConfigureRemoteTarget.ps1
@@ -6,6 +6,7 @@ If ($winRM.Status -ne "Running")
 {
     Start-Service -Name winrm
 }
+
 # Only change the PowerShell execution policy if we need to
 if ((Get-ExecutionPolicy) -ne "Unrestricted" -and (Get-ExecutionPolicy) -ne "Bypass")
 {
@@ -16,10 +17,55 @@ else
 {
     Write-Host -ForegroundColor White " - PowerShell execution policy already set to `"$(Get-ExecutionPolicy)`"."
 }
+
 Enable-PSRemoting -Force
 Enable-WSManCredSSP -Role Server -Force | Out-Null
 # Increase the local memory limit to 1 GB
 Set-Item WSMan:\localhost\Shell\MaxMemoryPerShellMB 1024 -WarningAction SilentlyContinue
+Get-Item WSMan:\localhost\Shell\
 
 #Get out of this PowerShell process
 Stop-Process -Id $PID -Force
+
+Notes RCE 
+#Remoting 
+Enable-PSremoting
+Enable-PSremoting
+Get-Command Enable-PSremoting
+Get-Command *-PSremot* | Select Name 
+Disable-PSRemoting                                                                                                    
+Enable-PSRemoting 
+
+Help Enable-PSremoting  
+SYNTAX
+Enable-PSRemoting [-Force] [-SkipNetworkProfileCheck] 
+
+#Enable-WSmanCredSSP
+Get-Command *-WSmanCredSSP | Select Name 
+Disable-WSManCredSSP
+Enable-WSManCredSSP
+Get-WSManCredSSP 
+The machine is not configured to allow delegating fresh credentials. 
+This computer is not configured to receive credentials from a remote client computer.
+
+Enable-WSManCredSSP
+The machine is not configured to allow delegating fresh credentials. 
+This computer is configured to receive credentials from a remote client computer.
+
+Help Enable-WSmanCredSSP 
+SYNTAX
+Enable-WSManCredSSP [-Role] <string> {Client | Server} [[-DelegateComputer] <string[]>] [-Force]  
+
+Enable-WSManCredSSP -Role Server -Force # | Out-Null
+cfg               : http://schemas.microsoft.com/wbem/wsman/1/config/service/auth
+lang              : en-US
+Basic             : false
+Kerberos          : true
+Negotiate         : true
+Certificate       : false
+CredSSP           : true
+CbtHardeningLevel : Relaxed
+
+Get-Item WSMan:\localhost\Shell\MaxMemoryPerShellMB
+
+
